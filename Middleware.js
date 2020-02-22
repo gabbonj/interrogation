f256de20e95cd0c413d1ddf47ee91bfff1c60a20
Gabriele Bongiorno <gabriele030403@gmail.com>
2020-02-22 18:33:19 +0100

    first API implementation

--- Middleware.js
+++ Middleware.js
@@ -1,5 +1,6 @@
 
 module.exports ={
+    // loggs the ip and the url of the request
     logger : (req, res, next) => {
         console.log(`Request from ${req.ip}, URL: ${req.url}`)
         next()
--- routers/apis.js
+++ routers/apis.js
@@ -2,19 +2,32 @@ const express = require('express')
 const router = express.Router()
 
 const MongoClient  = require('mongodb').MongoClient
-const url = 'mongodb://localhost:27017';
-const dbName = 'admin';
+const url = 'mongodb://localhost:27017'
+const dbName = 'interrogation'
 
+// debug
 router.get('/', (req, res) => {
     res.send('Welcome to the API')
 })
 
-router.get('/users', (req, res) => {
+// get all the calendars
+router.get('/interrogations', (req, res) => {
     MongoClient.connect(url).then((client) => {
-        var db = client.db('admin')
-        db.collection('organizzation').find().toArray(function(err, result) {
-            if (err) throw err;
-            console.log(result)
+        var db = client.db(dbName)
+        db.collection('calendars').find().toArray((err, result) => {
+            if (err) throw err
+            res.json(result)
+        })
+    })
+})
+
+// get calendars by subject
+router.get('/interrogations/subject/:name', (req, res) => {
+    const subject_name = req.params.name
+    MongoClient.connect(url).then((client) =>{
+        var db = client.db(dbName)
+        db.collection('calendars').find({'subject' : subject_name}).toArray((err, result) => {
+            if (err) throw err
             res.json(result)
         })
     })
