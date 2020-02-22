7d7b0f29143c1854a3cf4d9eedbf37c3e21b8b22
Gabriele Bongiorno <gabriele030403@gmail.com>
2020-02-22 17:23:03 +0100

    API bases

--- routers/apis.js
+++ routers/apis.js
@@ -1,6 +1,24 @@
 const express = require('express')
 const router = express.Router()
 
+const MongoClient  = require('mongodb').MongoClient
+const url = 'mongodb://localhost:27017';
+const dbName = 'admin';
+
+router.get('/', (req, res) => {
+    res.send('Welcome to the API')
+})
+
+router.get('/users', (req, res) => {
+    MongoClient.connect(url).then((client) => {
+        var db = client.db('admin')
+        db.collection('organizzation').find().toArray(function(err, result) {
+            if (err) throw err;
+            console.log(result)
+            res.json(result)
+        })
+    })
+})
 
 
 module.exports = router
